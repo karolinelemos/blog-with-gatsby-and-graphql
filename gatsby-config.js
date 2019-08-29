fdb0845e454fb5b8a6b86b0cb4c6a69f1fe7c6a9
karolinelemos <karolineclemos@hotmail.com>
2019-08-29 16:09:22 -0300

    Add Search config and Component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config()
+const queries = require('./src/utils/algolia_queries')
+
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
@@ -54,6 +57,17 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true, // default: false
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -62,7 +76,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,29 @@
-const { createFilePath } = require("gatsby-source-filesystem")
 const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// To add the slug field to each post 
-exports.onCreateNode = ({ node, actions, getNode }) => {
+// To add the slug field to each post
+exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-
+  // Ensures we are processing only markdown files
   if (node.internal.type === "MarkdownRemark") {
-    const slug = createFilePath({ 
+    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+    const slug = createFilePath({
       node,
       getNode,
-      basePath: "pages"
+      basePath: "pages",
     })
 
+    // Creates new query'able field with name of 'slug'
     createNodeField({
-      name: "slug",
       node,
+      name: "slug",
       value: `/${slug.slice(12)}`,
     })
   }
 }
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions 
-
+  const { createPage } = actions
   return graphql(`
     {
       allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
@@ -40,7 +41,7 @@ exports.createPages = ({ graphql, actions }) => {
             }
             timeToRead
           }
-          previous {
+          next {
             frontmatter {
               title
             }
@@ -48,12 +49,12 @@ exports.createPages = ({ graphql, actions }) => {
               slug
             }
           }
-          next {
-            frontmatter {
-              title
-            }
+          previous {
             fields {
               slug
+            }
+            frontmatter {
+              title
             }
           }
         }
@@ -65,12 +66,14 @@ exports.createPages = ({ graphql, actions }) => {
     posts.forEach(({ node, next, previous }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve("./src/templates/blog-post.js"),
+        component: path.resolve(`./src/templates/blog-post.js`),
         context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
           slug: node.fields.slug,
           previousPost: next,
-          nextPost: previous
-        }
+          nextPost: previous,
+        },
       })
     })
 
@@ -80,7 +83,7 @@ exports.createPages = ({ graphql, actions }) => {
     Array.from({ length: numPages }).forEach((_, index) => {
       createPage({
         path: index === 0 ? `/` : `/page/${index + 1}`,
-        component: path.resolve("./src/templates/blog-list.js"),
+        component: path.resolve(`./src/templates/blog-list.js`),
         context: {
           limit: postsPerPage,
           skip: index * postsPerPage,
