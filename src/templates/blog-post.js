39cc2f6ed4d27a31c09cafb6cb66af95a0b07cf1
karolinelemos <karolineclemos@hotmail.com>
2019-08-29 00:05:51 -0300

    Fix and add images on posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,29 +1,43 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import * as S from "../components/Post/styled"
 
 const BlogPost = ({ data }) => {
-    const {
-      markdownRemark: {
-        frontmatter: { title },
-        html
-      },
-    } = useStaticQuery(graphql`
-      query Post($slug: String) {
-        markdownRemark(fields: {slug: { eq: $slug }}) {
-          frontmatter {
-            title
-          }
-          html
-        }
-      }
-  `)
+  const post = data.markdownRemark
 
   return (
-    <>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{__html: html}}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+    </Layout>
   )
 }
 
-export default BlogPost
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+      }
+      html
+      timeToRead
+    }
+  }
+`
+
+export default BlogPosts
